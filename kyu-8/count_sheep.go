19c8a2d172b41a2a68dbef674de98f4afe616f43
Olleh-hellO <o.zholtani@gmail.com>
2022-08-01 22:50:08 +0300

    new task

---
+++ kyu-8/count_sheep.go
@@ -0,0 +1,22 @@
+/*
+Description:
+If you can't sleep, just count sheep!!
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
+package kata
+
+import "fmt"
+
+func countSheep(num int) string {
+
+	sheep := ""
+
+	for i := 0; i < num; i++ {
+
+		sheep += fmt.Sprintf("%v sheep...", i+1)
+	}
+
+	return sheep
+}
