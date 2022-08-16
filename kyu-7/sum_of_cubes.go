48921cf20e7bf10d4d269ebf187b1391d21c6b45
Olleh-hellO <o.zholtani@gmail.com>
2022-08-16 22:08:11 +0300

    new task

---
+++ kyu-7/sum_of_cubes.go
@@ -0,0 +1,20 @@
+/*Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+Assume that the input n will always be a positive integer.
+
+Examples: (Input --> output)
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)*/
+
+package kata
+
+func SumCubes(n int) int {
+
+	sumOfCubes := 0
+
+	for i := 1; i <= n; i++ {
+
+		sumOfCubes += (i * i * i)
+	}
+
+	return sumOfCubes
+}
